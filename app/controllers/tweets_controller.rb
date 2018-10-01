b4caa7a412411f282025aee14c6cd9595eebadc1
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-10-01 14:10:25 +0900

    投稿保存

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,16 @@ class TweetsController < ApplicationController
 	  @tweets = Tweet.all
 	end
 
+	def new
+	end
+
+	def create
+       Tweet.create(tweet_params)
+    end
+
+    private
+    def tweet_params
+      params.permit(:name, :image, :text)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  get 'tweets' => 'tweets#index'
+  get 'tweets' 		=> 'tweets#index'
+  get 'tweets/new'  =>  'tweets#new'
+  post 'tweets'     =>  'tweets#create'
 end
