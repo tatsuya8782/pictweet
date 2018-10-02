964aadfad57fae9b05f641b34b9372b096d8715d
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-10-03 01:54:56 +0900

    login

--- Gemfile
+++ Gemfile
@@ -49,3 +49,4 @@ end
  gem 'compass-rails','~> 2.0'
  gem 'sprockets', '2.11.0'
  gem 'kaminari'
+ gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'tweets' 		=> 'tweets#index'
   get 'tweets/new'  =>  'tweets#new'
   post 'tweets'     =>  'tweets#create'
