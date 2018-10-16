2d5ab11b0f6a55b300a8863df128f3b3b53460cd
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-10-16 12:24:29 +0900

    delete

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,23 +1,31 @@
-class TweetsController < ApplicationController
+cclass TweetsController < ApplicationController
 
-	before_action :move_to_index, except: :index
+    before_action :move_to_index, except: :index
 
-	def index
-	  @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
-	end
+    def index
+      @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
+    end
 
-	def new
-	end
+    def new
+    end
 
-	def create
-       Tweet.create(name: tweet_params[:name], image: tweet_params[:image], text: tweet_params[:text])
+    def create
+      Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+    end
+
+    def destroy
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+        tweet.destroy
+      end
     end
 
     private
     def tweet_params
-      params.permit(:name, :image, :text)
+      params.permit(:image, :text)
     end
-    
 
-  
-end
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
+  end
