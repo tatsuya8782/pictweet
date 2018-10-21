c144edcb3848581882142443b6e33831a68ad430
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-10-21 12:28:34 +0900

    comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class CommentsController < ApplicationController
-	def create
-    Comment.create(text: params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
-  en
+  def create
+    Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :tweet_id)
+  end
 end
