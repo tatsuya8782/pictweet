5978905a1f503f130040ee77a985fab8b8410a31
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-10-24 08:28:59 +0900

    tweet_controlle

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
 
-    before_action :move_to_index, except: :index
+    before_action :move_to_index, except: [:index, :show]
 
     def index
       @tweets = Tweet.includes(:user).page(params[:page]).per(5).order("created_at DESC")
@@ -42,3 +42,4 @@ class TweetsController < ApplicationController
     def move_to_index
       redirect_to action: :index unless user_signed_in?
     end
+  end
