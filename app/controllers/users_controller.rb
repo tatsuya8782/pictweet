4a2a545ac3253d9bb48df903dd5b1fe219278a18
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-11-16 20:17:38 +0900

    users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
+    @tweets = user.tweets.page(params[:page]).order("created_at DESC").per(5)
   end
 end
