65d5c4eb97aae912fd3e9107641e0ba425a320b1
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-11-23 23:50:26 +0900

    coments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
    @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
     redirect_to "/tweets/#{@comment.tweet.id}"   
    end 
+   
   private
   def comment_params
     params.permit(:text, :tweet_id)
